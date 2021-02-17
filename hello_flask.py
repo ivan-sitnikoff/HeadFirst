ef49b2bb26b6bcec93ba4c58bde5a524f85f0816
Ivan Sitnikov <sitnikov.ivan@physics.msu.ru>
2021-02-17 09:37:45 +0300

    Pt05: search4letters on the web. V.0

--- hello_flask.py
+++ hello_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from vsearch import search4letters
 
@@ -11,5 +11,10 @@ def hello() -> str:
 @app.route('/search')
 def do_search() -> str:
     return str(search4letters('life, universe and everything', 'eiru,l'))    
+
+@app.route('/entry')
+def entry_page():
+    return render_template('entry.html', 
+                           the_title='Welcome to search4letter on the web!')
 
 app.run()
